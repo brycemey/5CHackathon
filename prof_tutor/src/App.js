5c3f5085e19f4642ba93e9692b9183ce7405034a
brycemey <bmey2002@gmail.com>
2025-04-05 18:45:49 -0700

    Initialize React

---
+++ prof_tutor/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section>
+      
+        <h1>Here is the Beginner's Guide to Understanding Chat-GPT</h1>
+        <ul style={{backgroundColor: 'black',color: 'white'}}>
+        <li>First Step:</li>
+        </ul>
+        
+
+        
+    </section>
+  );
+}
+
+export default App;
